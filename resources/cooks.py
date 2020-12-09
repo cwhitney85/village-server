0e99428b523a457c2216b9c2c47b69b7877e2064
Colin Whitney <colin.whitney@gmail.com>
2020-12-09 12:42:35 -0500

    changed url prefix for cooks api to specify version 1

--- resources/cooks.py
+++ resources/cooks.py
@@ -5,7 +5,7 @@ from flask import Blueprint, jsonify, request
 from playhouse.shortcuts import model_to_dict
 
 
-cook = Blueprint('cooks', __name__, url_prefix='/cooks')
+cook = Blueprint('cooks', __name__, url_prefix='/api/v1/cooks')
 
 @cook.route('/')
 def index():
