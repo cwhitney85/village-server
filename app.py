fc31c4fddff4f3f12eb381904304f0c275d7697f
Colin Whitney <colin.whitney@gmail.com>
2020-12-13 14:17:44 -0500

    register route working and adding to the users table

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, jsonify, g
 from flask_cors import CORS
-
-import models
-from resources.cooks import cook
+from flask_login import LoginManager
 
 DEBUG = True
 PORT = 8000
 
+import models
+from resources.cooks import cook
+from resources.users import user
+
+login_manager = LoginManager()
+
 # Initialize the Flask class instance
 app = Flask(__name__)
+
+# Set up secret key and initiate sessions
+app.secret_key = "BABYWYATT"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(userid):
+  try:
+    return models.Users.get(models.Users.id == userid)
+  except models.DoesNotExist:
+    return None
 
 # Access database globally and connect before each request
 @app.before_request
@@ -28,8 +43,10 @@ def index():
   return "<h1>Let's Get Cookin'</h1>"
 
 CORS(cook, origins='*', supports_credentials=True)
-
 app.register_blueprint(cook)
+
+CORS(user, origins='*', supports_credentials=True)
+app.register_blueprint(user)
 
 # Start the app and create tables
 if __name__ == '__main__':
--- resources/users.py
+++ resources/users.py
@@ -6,3 +6,29 @@ from flask_login import login_user, current_user
 from playhouse.shortcuts import model_to_dict
 
 # Define Users Blueprint
+user = Blueprint('users', __name__, url_prefix='/user')
+
+# Register a new user
+@user.route('/register', methods=["POST"])
+def register():
+  payload = request.get_json()
+
+  payload['email'] = payload['email'].lower()
+  try:
+    # Check if user exists
+    models.Users.get(models.Users.email == payload['email'])
+    return jsonify(data={}, status={"code": 401, "message": "A user with that name already exists"})
+  except models.DoesNotExist:
+    # If user does not exist, create a new  one
+    payload['password'] =  generate_password_hash(payload['password'])
+    user = models.Users.create(**payload)
+
+    # Then log them in
+    login_user(user)
+
+    user_dict = model_to_dict(user)
+    print(user_dict)
+    print(type(user_dict))
+    del user_dict['password']
+
+    return jsonify(data=user_dict, status={"code": 201, "message": "Success"})
